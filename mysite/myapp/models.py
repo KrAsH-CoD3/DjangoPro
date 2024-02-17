cc9e80cf2f7ac7fd3912189f70db5d586d08b898
KrAsH-CoD3 <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-02-17 21:32:00 +0100

    UPDATED: Added Comments in model to unders. better

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -5,6 +5,8 @@ class Products(models.Model):
     def __str__(self) -> str:
         return self.name
     
+    # The variables below is the `id` and `name` in the each of the form values which should be the same.
+    # The `id` and `name` must have the variable name. IT A MUST.
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     desc = models.CharField(max_length=200)
