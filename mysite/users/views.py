cd85d9246b40e63d4cd866d4a3765212bc82cadd
KrAsH-CoD3 <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-02-18 16:37:22 +0100

    UPDATED: Register function in View.py

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -3,6 +3,10 @@ from .forms import NewUserForm
 
 # Create your views here.
 def register(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            pass
     form = NewUserForm()
     context = {
         'form' : form,
