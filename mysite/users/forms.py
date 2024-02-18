9e5b2002c7478a4d07b87282f4da6fa3244e5556
KrAsH-CoD3 <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-02-18 16:17:05 +0100

    UPDATED: Added placeholder for password fields in register form(forms.py)

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -5,8 +5,8 @@ from django.contrib.auth.forms import UserCreationForm
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class':'focus:outline-none', 'placeholder':'Demo@email.com'}))
     username = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'focus:outline-none', 'placeholder':'user1234'}))
-    password1 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class':'focus:outline-none'}))
-    password2 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class':'focus:outline-none'}))
+    password1 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class':'focus:outline-none', 'placeholder':'********'}))
+    password2 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class':'focus:outline-none', 'placeholder':'********'}))
 
     class Meta:
         model = User
