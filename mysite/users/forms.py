2f31524cf6559b23a06b269fa87d7d5c49e60dc6
KrAsH-CoD3 <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-02-18 16:54:35 +0100

    UPDATED: Added save function in forms.py

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -11,3 +11,10 @@ class NewUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("email", "username", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import NewUserForm
 
 # Create your views here.
 def register(request):
-    if request.method == "POST":
-        form = NewUserForm(request.POST)
-        if form.is_valid():
-            pass
     form = NewUserForm()
     context = {
         'form' : form,
     }
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('/myapp/products')
     return render(request, 'users/register.html', context)
