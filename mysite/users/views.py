33ee58dfd455e96ba313614488e1eae9b8786fa5
KrAsH-CoD3 <44311301+KrAsH-CoD3@users.noreply.github.com>
2024-02-19 13:26:27 +0100

    UPDATED: Refactor code

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -3,13 +3,14 @@ from .forms import NewUserForm
 
 # Create your views here.
 def register(request):
-    form = NewUserForm()
-    context = {
-        'form' : form,
-    }
     if request.method == "POST":
         form = NewUserForm(request.POST)
         if form.is_valid():
             user = form.save()
-            return redirect('/myapp/products')
+            return redirect('myapp/products')
+        else: print("The form is not VALID.")
+    form = NewUserForm()
+    context = {
+        'form' : form,
+    }
     return render(request, 'users/register.html', context)
